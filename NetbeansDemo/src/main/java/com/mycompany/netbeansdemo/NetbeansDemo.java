88f0330b1d01d30f4a18b04111ff7784d9d50afd
wmuha <wmuha@10.121.12.60>
2025-12-10 09:45:09 +0800

    first commit

---
+++ NetbeansDemo/src/main/java/com/mycompany/netbeansdemo/NetbeansDemo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.netbeansdemo;
+
+/**
+ *
+ * @author wan muhammad bin wan assim
+ */
+public class NetbeansDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
